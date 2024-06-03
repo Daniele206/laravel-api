40a8994996886a6203035ba742a1d709aa602366
Daniele206 <danieledeieso2004@gmail.com>
2024-06-03 16:24:09 +0200

    [add] two call api {technologies, types}

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -4,14 +4,30 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Technology;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function index(){
+    public function projects(){
 
         $projects = Project::with('type', 'technologies')->paginate(12);
 
         return response()->json($projects);
+    }
+
+    public function technologies(){
+
+        $technologies = Technology::with('projects')->get();
+
+        return response()->json($technologies);
+    }
+
+    public function types(){
+
+        $types = Type::with('projects')->get();
+
+        return response()->json($types);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/projects', [PageController::class, 'index']);
+Route::get('/projects', [PageController::class, 'projects']);
+Route::get('/technologies', [PageController::class, 'technologies']);
+Route::get('/types', [PageController::class, 'types']);
